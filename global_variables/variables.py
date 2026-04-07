67daafa67b4bde45141030fdde1d774cf201ad97
kuratorseksa <romanaroslavcev05@gmail.com>
2026-04-07 14:58:42 +0300

    Update variables.py

--- global_variables/variables.py
+++ global_variables/variables.py
@@ -4,36 +4,26 @@ from orm.models import CompetitionRules, CompetitionInteractions, CompetitionAdd
     CompetitionActivity, AlmostCurator
 from global_variables.token import api
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-import asyncio
 
 state_dispenser = BuiltinStateDispenser()
-labeler = BotLabeler()
 ctx = CtxStorage()
 deadline_scheduler = AsyncIOScheduler()
 photo_uploader = PhotoMessageUploader(api)
 doc_uploader = DocMessagesUploader(api)
-
+# labeler удалён отсюда
 user_accounting = set()
-
 green = KeyboardButtonColor.POSITIVE
 red = KeyboardButtonColor.NEGATIVE
 blue = KeyboardButtonColor.PRIMARY
-
 predsed_team_ids = [394608204, 520276059, 366048560, 466100052, 478135965, 348082199]
-
-
 comp_dict = {'activity': [CompetitionActivity, 'competition_activity_rate'],
              'responsibility': [CompetitionResponsibility, 'competition_responsibility_rate'],
              'additional': [CompetitionAdditional, 'competition_additional_rate'],
              'interactions': [CompetitionInteractions, 'competition_interactions_rate'],
-             'rules': [CompetitionRules, 'competition_rules_rate']
-             }
-
+             'rules': [CompetitionRules, 'competition_rules_rate']}
 top_tags_dict = {'По страйкам': 'strikes_number',
                  'По рейтингу': 'rating',
-                 'По пропускам': 'meeting_attendance'
-                 }
-
+                 'По пропускам': 'meeting_attendance'}
 predsed_team_dict = {
     '394608204': 'romas_rate',
     '520276059': 'ivans_rate',
