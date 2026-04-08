b1d6ff2f8c83595f48391aecd529163d626236aa
kuratorseksa <romanaroslavcev05@gmail.com>
2026-04-08 12:38:41 +0300

    Update database.py

--- orm/database.py
+++ orm/database.py
@@ -3,28 +3,22 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sess
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy import select, insert, update, delete, and_, create_engine, desc, asc
 
+# fix — Base объявляем ДО engine
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
 
 engine = create_async_engine(url=SQLALCHEMY_DATABASE_URL)
 Async_Session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 async_session = Async_Session()
 
-
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
     await engine.dispose()
-
 
 async def init_models():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
-
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
     await engine.dispose()
-
-
-class Base(AsyncAttrs, DeclarativeBase):
-    __abstract__ = True
